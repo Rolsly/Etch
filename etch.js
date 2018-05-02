1018e2c0df6972d0481cba4efe2a334520d98d11
Rolsly <jccooke@btinternet.com>
2018-05-02 13:00:28 +0100

    Third

--- etch.js
+++ etch.js
@@ -1,15 +1,18 @@
+let grid = prompt("How big do you want your grid?")
 
-for (j = 0; j < 100; j++){
+for (j = 0; j < Number(grid); j++){
   const container = document.querySelector('#container');
   const line = document.createElement('div');
   line.classList.add(j)
+  line.style.display ="block";
   container.appendChild(line);
 
-  for (i = 0; i < 100; i++){
+  for (i = 0; i < Number(grid); i++){
     document.querySelector('line');
     const square = document.createElement('span');
     square.classList.add(i)
     square.style.padding = "5px 5px 5px 5px";
+    square.style.margin = "0px 0px 0px 0px";
     square.style.display ="inline-block";
     square.style.border = "solid gray 1px";
     line.appendChild(square);
